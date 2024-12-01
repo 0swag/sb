0c9411151b5fc0591d1f98171436d7114ab2acfc
0swag <slav@insiberia.net>
2024-12-01 19:00:01 +0000

    fixes

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import os
+os.system('pip uninstall discord.py')
+os.system('pip uninstall discord.py-self')
 packages = ['discord.py-self', 'rgbprint', 'sys']
 for _ in packages:
     os.system(f'pip install {_}')
